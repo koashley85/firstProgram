3a9ca1908e4ecf2777de9a1c0dc092f0cd6f5622
Kavion <koashley85@gmail.com>
2024-09-11 23:32:20 -0400

    Basic Java Program
    Basic Java Program with output

---
+++ src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+        public static void main(String args[]) {
+                System.out.print("Hello there! ");
+System.out.println("My name is Kavion.");
+System.out.print("This is my first Java program.  ");
+System.out.println("Isnt It Great!!!");
+System.out.println("\"Yahooooo\"");
+}
+}
